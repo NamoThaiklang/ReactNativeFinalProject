a05e68c147e15276d8b0af1ed4da23b5f51bd8d0
NamoThaiklang <thaiklang555@gmail.com>
2022-10-20 17:00:47 +0700

    Update home+Change to Expo

---
+++ Phase3/ConfidentInsurance/component/Pro1.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View,SafeAreaView,Image } from 'react-native'
+import React from 'react'
+
+const Pro1 = () => {
+  return (
+    <SafeAreaView>
+    <View>
+      
+      <Text>Pro1</Text>
+    </View>
+    </SafeAreaView>
+  )
+}
+
+export default Pro1
+
+const styles = StyleSheet.create({})
